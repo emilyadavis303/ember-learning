e09ba4229062e0629de9d1fab939516d35555113
Emily Davis <emilyadavis303@gmail.com>
2014-11-25 10:28:02 -0700

    adds new note functionality

--- app/controllers/notes.js
+++ app/controllers/notes.js
@@ -6,19 +6,5 @@ export default Ember.ArrayController.extend({
     return this.get('content').length;
   }.property('content.[]'),
 
-  actions: {
 
-    addNote: function () {
-      var note = { title: 'New Note', body: 'Lorem ipsum . . .' };
-
-      $.post('/api/notes', note).done(function (data) {
-        this.get('content').pushObject(data);
-      }.bind(this)).fail(function (data) {
-        alert('Something went wrong with saving the note. Check the console.');
-        console.log('POST request failed!', data);
-      });
-    }
-
-  }
-  
 });
---
+++ app/controllers/notes/new.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  needs: ['notes'],
+
+  title: null,
+  body: null,
+
+  actions: {
+    saveNote: function() {
+      var title = this.get('title');
+      var body = this.get('body');
+      var note = { title: title, body: body };
+
+      Ember.$.post('/api/notes/', note).done(function () {
+        this.get('controllers.notes').get('content').pushObject(note);
+        this.set('message', 'Your note has been saved to the server.');
+        this.transitionToRoute('note', note);
+      }.bind(this));
+    }
+  }
+});
